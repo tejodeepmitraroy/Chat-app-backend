da56a03f132be358cd864dde96c5b67465f1f74f
unknown <tejodeepmitraroy2002@gmail.com>
2023-09-07 10:46:17 +0530

    ts wrote in production

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel";
+import { verifyJwtToken } from "../config/token/token";
 
 declare global {
   namespace Express {
@@ -28,9 +28,12 @@ export const authentication = asyncHandler(
         token = req.headers.authorization.split(" ")[1];
 
         //decode token id
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+        // const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+        const decoded: any = verifyJwtToken(token);
 
-        req.user = await User.findById(decoded.id).select("-password");
+        const user = await User.findById(decoded._id).select("-password");
+        req.user = user!;
+
         next();
       } catch (error) {
         res.status(401);
